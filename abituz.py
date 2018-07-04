7697ad69829fa313531f3cd68ec94be6674fa361
bcskda <demulkup@ya.ru>
2018-07-04 15:42:39 +0300

    flask shell support

--- abituz.py
+++ abituz.py
@@ -1,2 +1,12 @@
-from app import create_app
+from app import create_app, db
+from app.models import *
+
 app = create_app()
+
+@app.shell_context_processor
+def make_shell_context():
+	return {
+		'db': db, 'Application': Application,
+		'Student': Student, 'University': University,
+		'Faculty': Faculty, 'Program': Program
+	}
