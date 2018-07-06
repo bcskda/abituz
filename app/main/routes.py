58da68703c22afd984025cecf4af330add690f5f
bcskda <demulkup@ya.ru>
2018-07-06 10:52:06 +0300

    Add /count, /students/<name>

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,7 +1,7 @@
 from flask import redirect, url_for, request, render_template
 from sqlalchemy.orm.exc import NoResultFound
 from app import db
-from app.models import Student
+from app.models import Student, Application
 from app.main import bp
 
 @bp.route('/')
@@ -12,16 +12,26 @@ def redirect_to_index():
 def index():
 	return render_template('index.html', title='Home')
 
+@bp.route('/count')
+def count():
+	total = len(Application.query.all())
+	return render_template('count.html', count=total)
+
 @bp.route('/count/<name>')
-def count(name):
+def count_by_name(name):
 	try:
 		student = Student.query.filter_by(name=name).one()
 		count = len(student.applications)
 	except NoResultFound:
 		count = None
-	return render_template('count.html', title='Count', name=name, count=count)
+	return render_template('count_by_name.html', title='Count', name=name, count=count)
 
 @bp.route('/students')
-def students_all():
+def students():
 	students = Student.query.all()
 	return render_template('students.html', title='All students', students=students)
+
+@bp.route('/students/<name>')
+def student_by_name(name):
+	student = Student.query.filter_by(name=name).first_or_404()
+	return render_template('student_by_name.html', student=student)
