4f9bef13b3a430ad6731468886f081d1c2448286
bcskda <demulkup@ya.ru>
2018-07-11 16:50:42 +0300

    misc

--- app/config.py
+++ app/config.py
@@ -11,5 +11,5 @@ class Config(object):
 	APP_UPDATE_AUTO = (os.environ.get('APP_UPDATE_AUTO') not in [None, '0']) or \
 		False
 
-	APP_UPDATE_SECS = os.environ.get('APP_UPDATE_SECS') or \
-		1800 # 30 min
+	APP_UPDATE_SECS = int(os.environ.get('APP_UPDATE_SECS') or \
+		1800) # 30 min
